316ecdaeb42887e74fc6f11f00a2329920466e3d
L-SAGET <leo.saget@free.fr>
2020-01-06 16:37:47 +0100

    Add : Menu and front page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,111 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatDialog } from '@angular/material';
+import { MatSidenav } from '@angular/material/sidenav';
+
+import {
+  Router,
+  Event as RouterEvent,
+  NavigationStart,
+  NavigationEnd,
+  NavigationCancel,
+  NavigationError
+} from '@angular/router';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'difference-ours-train';
+export class AppComponent implements OnInit {
+  title = 'QELDE1OE1T?';
+  // title = 'Quelle est la différence entre un Ours et un Train ?';
+
+  mode = new FormControl('push');
+  loading = true;
+
+  // constructor(public dialog: MatDialog, private router: Router) {
+  //   router.events.subscribe((event: RouterEvent) => {
+  //     this.navigationInterceptor(event);
+  //   });
+  // }
+  navigationInterceptor(event: RouterEvent): void {
+    if (event instanceof NavigationStart) {
+      this.loading = true;
+    }
+    if (event instanceof NavigationEnd) {
+      this.loading = false;
+    }
+
+    if (event instanceof NavigationCancel) {
+      this.loading = false;
+    }
+    if (event instanceof NavigationError) {
+      this.loading = false;
+    }
+  }
+
+  random() {
+    //  this.router.navigateByUrl('random');
+  }
+  new() {
+    //  this.router.navigateByUrl('new');
+  }
+  authenticated() {
+    if (localStorage.getItem('LoggedIn') == null) {
+      return false;
+    }
+    if (localStorage.getItem('LoggedIn') != null) {
+      return true;
+    } else { return false; }
+  }
+
+  logout() {
+    console.log('clicked');
+    // const dialogRef = this.dialog.open(LogoutComponent, {
+    //   width: "430px"
+    // });
+    // dialogRef.afterClosed().subscribe(data => {
+    //   console.log("Logout closed");
+    //   console.log(data);
+    // });
+  }
+
+  openDialog() {
+    // console.log("clicked");
+    // const dialogRef = this.dialog.open(LoginComponent, {
+    //   width: "300px"
+    // });
+    // dialogRef.afterClosed().subscribe(data => {
+    //   console.log("Login closed");
+    //   console.log(data);
+    // });
+  }
+  openDialog1() {
+    // console.log("clicked");
+    // const dialogRef = this.dialog.open(SignupComponent, {
+    //   height: "auto",
+    //   width: "430px"
+    // });
+    // dialogRef.afterClosed().subscribe(data => {});
+  }
+
+  // public socialSignIn(socialPlatform : string) {
+  //   let socialPlatformProvider;
+  //   if(socialPlatform == "facebook"){
+  //     socialPlatformProvider = FacebookLoginProvider.PROVIDER_ID;
+  //   }else if(socialPlatform == "google"){
+  //     socialPlatformProvider = GoogleLoginProvider.PROVIDER_ID;
+  //   }
+
+  //   this.socialAuthService.signIn(socialPlatformProvider).then(
+  //     (userData) => {
+  //       console.log(socialPlatform+" sign in data : " , userData);      }
+  //   );
+  // }
+
+  ngOnInit(): void {
+    this.authenticated();
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCardModule, MatIconModule, MatSidenavModule, MatToolbarModule} from '@angular/material';
+import { DifferenceComponent } from './components/difference/difference.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DifferenceComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
