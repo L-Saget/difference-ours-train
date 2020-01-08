33f727c05ce84ede8b28c4bbd7ffd6464aac7e36
Leo Saget <leo.saget@free.fr>
2020-01-08 21:37:04 +0100

    fix config prod

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   endpoints: {
-    differences: 'http://localhost:3333/differences',
+    differences: 'https://back-adonis.herokuapp.com/differences',
   }
 };
 
